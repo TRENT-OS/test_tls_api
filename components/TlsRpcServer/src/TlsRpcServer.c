f3cc168993ea155889a0a582ee77faa59beb274d
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-07-21 14:04:20 +0200

    [REN] Align naming of CAmkES names for entropy port/rpc

--- components/TlsRpcServer/src/TlsRpcServer.c
+++ components/TlsRpcServer/src/TlsRpcServer.c
@@ -57,8 +57,8 @@ static OS_Crypto_Config_t cryptoCfg =
 {
     .mode = OS_Crypto_MODE_LIBRARY_ONLY,
     .library.entropy = OS_CRYPTO_ASSIGN_EntropySource(
-        entropySource_rpc,
-        entropySource_dp),
+        entropy_rpc,
+        entropy_port),
 };
 static OS_Network_Socket_t socketCfg =
 {
