6bc759a52722d0c3a1af72c480dfa89ab1a9658d
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-03-19 00:37:49 +0100

    [DOC] align file headers

--- components/NwStack/src/network_stack.c
+++ components/NwStack/src/network_stack.c
@@ -1,8 +1,7 @@
 /*
- *  OS Network Stack CAmkES wrapper
+ * Network Stack CAmkES wrapper
  *
- *  Copyright (C) 2019, Hensoldt Cyber GmbH
- *
+ * Copyright (C) 2019-2021, HENSOLDT Cyber GmbH
  */
 
 #include "SystemConfig.h"
