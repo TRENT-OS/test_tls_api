16ae389e345a9736e8fad6ab0b1bc2139151b30b
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-04-14 21:41:17 +0200

    [REF] Unify dataports' names
    Our guidelines are enforcing 'port' suffix when naming dataports.
    
    See:
    SEOS-1626

--- components/Tests/src/test_OS_Tls.c
+++ components/Tests/src/test_OS_Tls.c
@@ -603,7 +603,7 @@ static void
 init_network_client_api()
 {
     static OS_NetworkStackClient_SocketDataports_t config;
-    static OS_Dataport_t dataport = OS_DATAPORT_ASSIGN(NwAppDataPort);
+    static OS_Dataport_t dataport = OS_DATAPORT_ASSIGN(networkStack_port);
 
     config.number_of_sockets = 1;
 
